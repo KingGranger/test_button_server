33ad957a6022ce62b72473ac8bf9ee2d0fc49e05
KingGranger <cheeridon@gmail.com>
2018-05-26 12:32:42 -0400

    created raffle method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,8 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create(user_params)
-
+    #raffle = @user.get_random_ticket_number(@user.id, @user.phone_number)
+    #first_name: @user.first_name, last_name: @user.last_name, phone_number: @user.phone_number, raffle: raffle
     render json: @user
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+
+  def get_random_ticket_number id, phone_number
+    alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    prefix = id <= 26 ? alphabet[id-1]: alphabet[(id % 26) -1]
+    number = phone_number.to_s.split("").map{|i| i.to_i}.inject(:+) * id
+    prefix + number.to_s
+  end
+  #have to create a raffle class where the raffle belongs to the user and the user has one raffle
 end
