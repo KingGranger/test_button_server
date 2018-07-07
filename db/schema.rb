209c734c10f3320f3178e09c20a804c955406532
KingGranger <cheeridon@gmail.com>
2018-07-07 16:50:42 -0400

    updated users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180624213840) do
+ActiveRecord::Schema.define(version: 20180707203953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,6 +78,8 @@ ActiveRecord::Schema.define(version: 20180624213840) do
     t.boolean "terms_of_agreement"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
+    t.string "email"
   end
 
   add_foreign_key "authentications", "users"
